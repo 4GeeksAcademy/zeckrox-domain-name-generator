133b59339c9627fab952ff29a5436f478caa20d4
Stefano <tefanoboschetti@gmail.com>
2023-08-05 12:42:06 +0000

    First commit on excuse generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,70 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+var pronoun = ["el", "un", "tu", "ese", "aquel", "mi", "nuestro"];
+var adj = [
+  "gran",
+  "enorme",
+  "pequeño",
+  "supersticioso",
+  "orgulloso",
+  "curioso",
+  "lindo",
+  "gracioso",
+  "cobarde",
+  "tonto"
+];
+var noun = [
+  "gato",
+  "elefante",
+  "hombre",
+  "pantalon",
+  "humano",
+  "edificio",
+  "carro",
+  "piñata",
+  "chocolate"
+];
+var ext = [
+  ".com",
+  ".net",
+  ".es",
+  ".org",
+  ".gg",
+  ".co",
+  ".ve",
+  ".io",
+  ".info",
+  ".com.ve"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let domainsListed = ["<tr>"];
+let htmlList = document.getElementById("domainList");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// START Funciones generadoras de dominios
+const domainGenerator = () => {
+  let randomPronoun = pronoun[parseInt(Math.random() * pronoun.length)];
+  let randomAdj = adj[parseInt(Math.random() * adj.length)];
+  let randomNoun = noun[parseInt(Math.random() * noun.length)];
+  let randomExt = ext[parseInt(Math.random() * ext.length)];
+
+  let domain =
+    "<td>" + randomPronoun + randomAdj + randomNoun + randomExt + "</td>";
+
+  return domain;
 };
+
+const printDomains = () => {
+  for (let counter = 0; counter < 40; counter++) {
+    let x = domainGenerator();
+    if (counter % 4 == 0 && counter !== 0) {
+      domainsListed += "</tr><tr>";
+    }
+    domainsListed += x;
+  }
+};
+// END Funciones generadoras de dominios
+
+function onload() {
+  printDomains();
+  htmlList.innerHTML = domainsListed;
+}
+
+onload();
